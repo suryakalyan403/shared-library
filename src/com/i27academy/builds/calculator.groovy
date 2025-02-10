48214fe00f42846cc0c0137141a201b90ebafb91
suryakalyan403@gmail.com <kalyansurya403@gmail.com>
2025-02-10 12:42:13 -0500

    Added the New Shared Library Package

---
+++ src/com/i27academy/builds/calculator.groovy
@@ -0,0 +1,44 @@
+package com.i27academy.builds;
+
+class Calculator {
+
+      def jenkins
+
+      Calculator(jenkins){
+
+         this.jenkins = jenkins
+
+      }
+
+
+      //Addition Method
+      def add(n1, n2){
+
+          return n1 + n2
+
+      }
+
+      //Subtraction Method
+      def sub(n1, n2){
+
+          return n1 - n2
+
+      }
+
+      //Multiplication Method
+      def mul(n1,n2){
+
+          return n1 * n2
+
+      }
+
+      //Division Method
+      def div(n1, n2){
+
+          return n1/n2
+
+      }
+
+
+}
+

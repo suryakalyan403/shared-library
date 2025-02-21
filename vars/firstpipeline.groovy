c84d165d4dd228951407635f69b6d39942c3f351
suryakalyan403@gmail.com <kalyansurya403@gmail.com>
2025-02-21 04:56:29 -0500

    Added the configurable parameteres in the Pipeline

--- vars/firstpipeline.groovy
+++ vars/firstpipeline.groovy
@@ -3,20 +3,24 @@ import com.i27academy.builds.Calculator
 def call(Map pipelineparams) {
     pipeline {
         agent any
+ 
+        //Paramter Definition
+        parameters{
+         string(name: 'IMG_FILEPATH', defaultValue: "/var/lib/jenkins/workspace/images", description:"Path where Img exists")
+         string(name: 'IMG_FILENAME', defaultValue: "base-0.4.9.tar.gz", description:"Img Name")
+
+        }
         stages {
-            stage('Addition') {
+            stage('Docker Img Extract') {
                 steps {
                     script {
                         // Pass 'steps' to the Calculator class
                         Calculator calculator = new Calculator(steps)
 
                         // Call the dockerImgExtract method
-                        def result = calculator.dockerImgExtract("/var/lib/jenkins/workspace/images", "base-0.4.9.tar.gz")
+                        def result = calculator.dockerImgExtract(params.IMG_FILEPATH, params.IMG_FILENAME)
                         echo result
 
-                        // Call the add method
-                        echo "Sum: ${calculator.add(3, 4)}"
-                        echo "Microservice: ${pipelineparams.APP_NAME}"
                     }
                 }
             }

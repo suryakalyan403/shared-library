19449a6c22cd1590e85e63100a880a6e48dbaf76
Skalyan <kalyansurya403@gmail.com>
2025-09-05 16:16:02 +0000

    Updated the grrovy pipeline code

---
+++ vars/firstPipeline.groovy
@@ -0,0 +1,29 @@
+import com.eureka.builds.Calculator
+
+def call(Map pipelineparams) {
+    pipeline {
+        agent {
+        label "k8s-jenkins-slave" }
+
+        environment {
+
+            APP_NAME = "${pipelineparams.appName}"
+
+        } 
+        stages {
+            stage('AdditionStage') {
+                steps {
+                    script {
+                        echo "********** Test **********"
+                        // Use withCredentials to securely inject Docker credentials
+                        echo "Printing Sum of 2 Numbers"
+                        println calculator.add(3,4)
+                        echo "******** Microservice Name is: ${APP_NAme}"
+                        
+                        }
+                    }
+                }
+            }
+        }
+    }
+

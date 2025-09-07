5e2d9ee199e440191071fcb25de139ffd4726ea4
Skalyan <kalyansurya403@gmail.com>
2025-09-07 11:23:51 +0000

    Updated the Env variable

--- vars/firstPipeline.groovy
+++ vars/firstPipeline.groovy
@@ -18,7 +18,7 @@ def call(Map pipelineparams) {
         }
 
         environment {
-            APPLICATION_NAME = "${pipelineParams.appName}"
+            //APPLICATION_NAME = "${pipelineParams.appName}"
 
             // SonarQube
             SONAR_TOKEN = credentials('sonar_creds')
